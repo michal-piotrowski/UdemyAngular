915699a8c9ce4a4a683251177c6211afbe1c84a3
Michał Piotrowski <Mi.Piotrowski@post.com>
2020-01-25 12:54:29 +0100

    Second assignment udemy bindings exercvice

--- basics-components/src/app/app.module.ts
+++ basics-components/src/app/app.module.ts
@@ -6,16 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import {WarningComponent} from './warningAlert/warning.component';
-import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    WarningComponent,
-    SuccessComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule,
