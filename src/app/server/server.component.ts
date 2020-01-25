66df73b797656df59d849918377022577cda8b7e
Michał Piotrowski <Mi.Piotrowski@post.com>
2020-01-25 10:53:58 +0100

    First Exercise Udemy

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component(
+  {
+    selector: 'app-server',
+    templateUrl: 'server.component.html',
+    styleUrls: ['./servers.component.css']
+
+  }
+)
+export class ServerComponent {
+
+}
